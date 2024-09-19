b00fde3f72d30db34a20294c8e7416c59157d911
Mark <markmarkmagdy1010@gmail.com>
2024-09-19 12:51:13 +0300

    create seperate file for myApp

---
+++ lib/app/myApp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+    );
+  }
+}
