41d416f638fa617e96df9794b803970cb7aebc5e
Mark <markmarkmagdy1010@gmail.com>
2024-09-19 19:13:00 +0300

    add styles file

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,6 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/core/utils/color_manager.dart';
 
 abstract class Styles {
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
 
-  static FontStyle fontStyle
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyleLogo = TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const textStyle42 = TextStyle(
+    fontSize: 42,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'BebasNeue',
+  );
 }
