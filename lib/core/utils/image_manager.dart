b42b3821c82ff5514b8b9055e4bdd30a549a7cff
Mark <markmarkmagdy1010@gmail.com>
2024-09-20 19:55:04 +0300

    add json images

--- lib/core/utils/image_manager.dart
+++ lib/core/utils/image_manager.dart
@@ -5,4 +5,12 @@ class ImageManager {
   static String cloudGif = "assets/images/icons/cloud.gif";
 
   static String searchGif = "assets/images/icons/search.gif";
+
+  static String earthJson = "assets/images/icons/earth2.json";
+
+  static String worldJson = "assets/images/icons/world.json";
+
+  static String noEntryGif = "assets/images/icons/noEntry.gif";
+
+  static String fireworkGif = "assets/images/icons/firework.gif";
 }
