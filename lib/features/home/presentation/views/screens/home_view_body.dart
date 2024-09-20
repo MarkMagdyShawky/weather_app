97e0802de05ab4c991b3c804e3a329633cbc73a0
Mark <markmarkmagdy1010@gmail.com>
2024-09-20 19:59:20 +0300

    add all components to home view body

---
+++ lib/features/home/presentation/views/screens/home_view_body.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/core/utils/color_manager.dart';
+import 'package:weather_app/core/utils/gradient_manager.dart';
+import 'package:weather_app/core/utils/image_manager.dart';
+import 'package:weather_app/core/utils/padding_manager.dart';
+import 'package:weather_app/core/utils/pageDimensions.dart';
+import 'package:lottie/lottie.dart';
+
+import 'package:weather_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:weather_app/features/home/presentation/views/widgets/custom_home_section2.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: PageDimensions().pageWidth(context),
+      height: PageDimensions().pageHeight(context),
+      decoration: BoxDecoration(
+        gradient: LinearGradientbackground(kMainHomeBackgroundColorList),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: kPadding40, right: kPadding20, left: kPadding20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            CustomAppBar(),
+            Lottie.asset(ImageManager.worldJson),
+            CustomHomeSection2(),
+          ],
+        ),
+      ),
+    );
+  }
+}
