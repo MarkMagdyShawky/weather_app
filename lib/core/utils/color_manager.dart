d8bb160b525dcc14bbd4d85030a29b1ae3771b47
Mark <markmarkmagdy1010@gmail.com>
2024-09-20 19:55:17 +0300

    add new color

--- lib/core/utils/color_manager.dart
+++ lib/core/utils/color_manager.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 const kMainHomeBackgroundColorList = [kDesertColor4, kDesertColor3, kDesertColor2, kDesertColor1];
 const kSplashBackgroundColorList = [kSecondColor, kPrimaryColor];
 
-const kDesertColor1 = Color(0xffFDDEAB);
-const kDesertColor2 = Color(0xffFCDCAC);
-const kDesertColor3 = Color(0xffFEF5C8);
-const kDesertColor4 = Color(0xffFDEBD3);
+const Color kDesertColor1 = Color(0xffFDDEAB);
+const Color kDesertColor2 = Color(0xffFCDCAC);
+const Color kDesertColor3 = Color(0xffFEF5C8);
+const Color kDesertColor4 = Color(0xffFDEBD3);
+
+const Color kGreenColor = Color(0xff143F45);
 
 const Color kPrimaryColor = Color(0xff2680ef);
 const Color kSecondColor = Color(0xff72dffd);
+
 const Color kFontWhite = Colors.white;
