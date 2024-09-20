da921137deeedb4aceaaf6d90947e58a77687480
Mark <markmarkmagdy1010@gmail.com>
2024-09-20 14:22:11 +0300

    create custom button

---
+++ lib/core/widget/custom_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/core/utils/styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    this.borderRadius,
+    required this.text,
+    this.onPressed,
+  });
+
+  final Color backgroundColor;
+  final Color textColor;
+  final BorderRadius? borderRadius;
+  final String text;
+  final Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ?? BorderRadius.circular(15),
+          ),
+          backgroundColor: backgroundColor,
+          foregroundColor: textColor,
+          textStyle: Styles.textStyle18.copyWith(
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(color: textColor, fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
