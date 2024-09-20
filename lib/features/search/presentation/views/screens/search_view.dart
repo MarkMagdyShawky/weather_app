c030721a2ac5dffc01275c65057cbdecd18c872c
Mark <markmarkmagdy1010@gmail.com>
2024-09-20 23:04:11 +0300

    search view startup

---
+++ lib/features/search/presentation/views/screens/search_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/features/search/presentation/views/screens/search_view_body.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SearchViewBody(),
+    );
+  }
+}
