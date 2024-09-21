0c326ae63a076d1aa06be5e045b8ba15919a320a
Mark <markmarkmagdy1010@gmail.com>
2024-09-22 00:01:45 +0300

    create weather views

---
+++ lib/features/home/presentation/views/widgets/weather_views/thunderstorm_view_body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/core/utils/color_manager.dart';
+import 'package:weather_app/core/utils/gradient_manager.dart';
+import 'package:weather_app/core/utils/padding_manager.dart';
+import 'package:weather_app/core/utils/pageDimensions.dart';
+import 'package:weather_app/features/home/presentation/views/widgets/custom_weather_app_bar.dart';
+
+class ThunderstormViewBody extends StatelessWidget {
+  const ThunderstormViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: PageDimensions().pageWidth(context),
+      height: PageDimensions().pageHeight(context),
+      decoration: BoxDecoration(
+        gradient: LinearCenterGradientbackground(kThunderstormColorList),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: kPadding45, right: kPadding20, left: kPadding20),
+        child: Column(
+          children: <Widget>[
+            CustomWeatherAppBar(location: "San Fransisco", updatingTime: '11:00'),
+          ],
+        ),
+      ),
+    );
+  }
+}
