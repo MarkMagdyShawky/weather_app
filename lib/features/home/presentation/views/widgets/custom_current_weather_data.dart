f68097e49645f879a961f403abc3cba325f4e0c1
Mark <markmarkmagdy1010@gmail.com>
2024-09-22 04:12:54 +0300

    custom widget for the current weather data

---
+++ lib/features/home/presentation/views/widgets/custom_current_weather_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weather_app/core/utils/padding_manager.dart';
+
+import '../../../../../core/utils/styles.dart';
+
+class CustomCurrentWeatherData extends StatelessWidget {
+  const CustomCurrentWeatherData({
+    super.key,
+    required this.weatherState,
+    required this.currentTemp,
+    required this.minTemp,
+    required this.maxTemp, required this.textsColor,
+  });
+
+  final String weatherState;
+  final String currentTemp;
+  final String minTemp;
+  final String maxTemp;
+final Color textsColor;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          weatherState,
+          style: Styles.textStyle24.copyWith(fontFamily: 'PingFang SC',color: textsColor),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: kPadding10),
+        Text(
+          "$currentTemp°",
+          style: Styles.textStyle48
+              .copyWith(fontWeight: FontWeight.w600, fontFamily: 'Inter', color:textsColor ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: kPadding10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              "Min: $minTemp°",
+              style: Styles.textStyle22.copyWith(color: textsColor),
+              textAlign: TextAlign.center,
+
+            ),
+            Text(
+              "Max: $maxTemp°",
+              style: Styles.textStyle22.copyWith(color: textsColor),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
