dbf77f46819ec0ccaef0314c756d7af5247add2d
Mark <markmarkmagdy1010@gmail.com>
2024-09-23 18:52:32 +0300

    update api service

--- lib/core/service/api_service.dart
+++ lib/core/service/api_service.dart
@@ -1,12 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dio/dio.dart';
 
 class ApiService {
-  Dio _dio = Dio();
+  Dio dio = Dio();
 
   String baseUrl = 'http://api.weatherapi.com/v1/';
+  ApiService();
 
-  Future<Map<String, dynamic>> get(String endPoint) async {
-    var response = await _dio.get('$baseUrl$endPoint');
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await dio.get('$baseUrl$endPoint');
     return response.data;
   }
 }
