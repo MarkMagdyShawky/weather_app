9f7f5dc4c158db875d3eb3c9643f21d71df3b3c9
Mark <markmarkmagdy1010@gmail.com>
2024-09-23 18:53:08 +0300

    update weather cubit

--- lib/features/home/presentation/manager/weather_cubit/weather_cubit.dart
+++ lib/features/home/presentation/manager/weather_cubit/weather_cubit.dart
@@ -2,17 +2,17 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:weather_app/core/Models/weather_model/weather_model.dart';
 import 'package:weather_app/features/home/data/repos/home_repo.dart';
+import 'package:weather_app/features/home/data/repos/home_repo_impl.dart';
 
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit({required this.homeRepo}) : super(WeatherInitial());
-  HomeRepo homeRepo;
-
+  WeatherCubit() : super(WeatherInitial());
+  var result;
   Future<void> featchWeather({required String location}) async {
     emit(WeatherLoading());
 
-    var result = await homeRepo.featchWeather(location: location);
+    var result = await HomeRepoImpl().featchWeather(location);
 
     result.fold(
       (failure) {
